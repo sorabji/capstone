bc68e996b6f8d2015e3777033194920cb03fa39c
hp-laptop <sorabji52@gmail.com>
2011-11-13 20:45:31 -0800

    hrm

--- capstoneDB.sql
+++ capstoneDB.sql
@@ -1,130 +1,334 @@
-## build phone list
-## for mySQL
+-- phpMyAdmin SQL Dump
+-- version 3.3.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 13, 2011 at 08:35 PM
+-- Server version: 5.1.54
+-- PHP Version: 5.3.5-1ubuntu7.3
 
-USE capstone;
-DROP TABLE IF EXISTS questions;
-DROP TABLE IF EXISTS quizzes;
-DROP TABLE IF EXISTS instructors;
-DROP TABLE IF EXISTS students;
-DROP TABLE IF EXISTS people;
-DROP TABLE IF EXISTS sections;
-DROP TABLE IF EXISTS absences;
-DROP TABLE IF EXISTS assignments;
-DROP TABLE IF EXISTS submissions;
-DROP TABLE IF EXISTS grades;
-DROP TABLE IF EXISTS emails;
-DROP TABLE IF EXISTS courses;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-CREATE TABLE people (
-       id INT(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
-       first_name VARCHAR(15),
-       last_name VARCHAR (15),
-       address VARCHAR(50),
-       email VARCHAR(20),
-       phone VARCHAR(15),
-       social INT(9),
-       PRIMARY KEY(id))
-       ENGINE=INNODB;
 
-CREATE TABLE courses(
-       id VARCHAR(6) NOT NULL,
-       course_Desc VARCHAR(50),
-       PRIMARY KEY (id))
-       ENGINE=INNODB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-CREATE TABLE quizzes(
-       id INT(10) unsigned zerofill NOT NULL,
-       course_id VARCHAR(6) NOT NULL,
-       isOpen INT(1) NOT NULL,
-       FOREIGN KEY (course_id) REFERENCES courses(id),
-       PRIMARY KEY (id, course_id))
-       ENGINE=INNODB;
+--
+-- Database: `junk`
+--
 
-CREATE TABLE questions (
-       quiz_id INT(10) unsigned zerofill NOT NULL,
-       quest_num INT NOT NULL,
-       quest_txt VARCHAR(200) NOT NULL,
-       ansA VARCHAR(30),
-       ansB VARCHAR(30),
-       ansC VARCHAR(30),
-       ansD VARCHAR(30),
-       ansE VARCHAR(30),
-       correctAnswer CHAR,
-       FOREIGN KEY (quiz_id) REFERENCES quizzes(id),
-       PRIMARY KEY (quiz_id, quest_num))
-       ENGINE=INNODB;
+-- --------------------------------------------------------
 
-CREATE TABLE sections(
-       id INT(10) unsigned zerofill AUTO_INCREMENT NOT NULL
-       course_id VARCHAR(6) NOT NULL,
-       sec VARCHAR(6) NOT NULL,
-       FOREIGN KEY (course_id) REFERENCES courses(id),
-       PRIMARY KEY (course_id, sec))
-       ENGINE=INNODB;
+--
+-- Table structure for table `absences`
+--
 
-CREATE TABLE instructors (
-       instructor_id INT(10) unsigned zerofill NOT NULL,
-       sec_id 
-       FOREIGN KEY (instructor_id) REFERENCES people(id),
-       FOREIGN KEY (sec_id) REFERENCES sections(id),
-       PRIMARY KEY (instructor_id, sec))
-       ENGINE=INNODB;
-       
-CREATE TABLE students (
-       student_id INT(10) unsigned zerofill NOT NULL,
-       sec VARCHAR(6) NOT NULL,
-       FOREIGN KEY (student_id) REFERENCES people(id),
-       FOREIGN KEY (sec) REFERENCES sections(sec)
-       PRIMARY KEY (student_id, sec))
-       ENGINE=INNODB;
+CREATE TABLE IF NOT EXISTS `absences` (
+  `sec_id` int(10) unsigned zerofill NOT NULL,
+  `student_id` int(10) unsigned zerofill NOT NULL,
+  `dates` date NOT NULL,
+  `isAbsent` int(1) NOT NULL,
+  `isExcused` int(1) NOT NULL,
+  PRIMARY KEY (`sec_id`,`student_id`,`dates`),
+  KEY `student_id` (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE absences(
-       sec VARCHAR(6) NOT NULL,
-       student_id INT(10) unsigned zerofill NOT NULL,
-       dates DATE,
-       isAbsent INT(1),
-       isExcused INT(1),
-       FOREIGN KEY (sec) REFERENCES sections(sec),
-       FOREIGN KEY (student_id) REFERENCES people(id),
-       PRIMARY KEY (sec, student_id, dates))
-       ENGINE=INNODB;
+--
+-- Dumping data for table `absences`
+--
 
-CREATE TABLE assignments(
-       id VARCHAR(6) NOT NULL,
-       course_id VARCHAR(6) NOT NULL,
-       points_poss INT(10) NOT NULL,
-       FOREIGN KEY (course_id) REFERENCES courses(id),
-       PRIMARY KEY (course_id, id))
-       ENGINE=INNODB;
 
-CREATE TABLE submissions(
-       sec VARCHAR(6) NOT NULL,
-       ass_num VARCHAR(6) NOT NULL,
-       student_id INT(10) unsigned zerofill NOT NULL,
-       points_received INT(10),
-       FOREIGN KEY (sec) REFERENCES sections(sec),
-       FOREIGN KEY (student_id) REFERENCES people(id),
-       FOREIGN KEY (ass_num) REFERENCES assignments(id),
-       PRIMARY KEY (course_id, student_id, ass_num))
-       ENGINE=INNODB;
+-- --------------------------------------------------------
 
-CREATE TABLE grades(
-       student_id INT(10) unsigned zerofill NOT NULL,
-       sec VARCHAR(6) NOT NULL,
-       points_poss INT(10),
-       points_received INT(10)
-       FOREIGN KEY (sec) REFERENCES sections(sec),
-       FOREIGN KEY (student_id) REFERENCES people(id),
-       PRIMARY KEY (student_id, sec))
-       ENGINE=INNODB;
+--
+-- Table structure for table `assignments`
+--
 
-CREATE TABLE emails(
-       id INT(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
-       person_id INT(10) unsigned zerofill NOT NULL,
-       parent INT(10) unsigned zerofill,
-       isRead INT(1),
-       bodyTxt VARCHAR(200),
-       FOREIGN KEY (person_id) REFERENCES people(id),
-       PRIMARY KEY (id, person_id))
-       ENGINE=INNODB;
+CREATE TABLE IF NOT EXISTS `assignments` (
+  `id` varchar(6) NOT NULL,
+  `course_id` varchar(6) NOT NULL,
+  `points_poss` int(10) NOT NULL,
+  PRIMARY KEY (`id`,`course_id`),
+  KEY `course_id` (`course_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `assignments`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE IF NOT EXISTS `courses` (
+  `id` varchar(6) NOT NULL,
+  `course_Desc` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `courses`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emails`
+--
+
+CREATE TABLE IF NOT EXISTS `emails` (
+  `id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `person_id` int(10) unsigned zerofill NOT NULL,
+  `parent` int(10) unsigned zerofill NOT NULL,
+  PRIMARY KEY (`id`,`person_id`),
+  KEY `parent` (`parent`),
+  KEY `person_id` (`person_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `emails`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grades`
+--
+
+CREATE TABLE IF NOT EXISTS `grades` (
+  `student_id` int(10) unsigned zerofill NOT NULL,
+  `sec_id` int(10) unsigned zerofill NOT NULL,
+  `points_poss` int(10) NOT NULL,
+  `points_received` int(11) NOT NULL,
+  PRIMARY KEY (`student_id`,`sec_id`),
+  KEY `sec_id` (`sec_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `grades`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instructors`
+--
+
+CREATE TABLE IF NOT EXISTS `instructors` (
+  `instructor_id` int(10) unsigned zerofill NOT NULL,
+  `sec_id` int(10) unsigned zerofill NOT NULL,
+  PRIMARY KEY (`instructor_id`,`sec_id`),
+  KEY `sec_id` (`sec_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `instructors`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `people`
+--
+
+CREATE TABLE IF NOT EXISTS `people` (
+  `id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(15) DEFAULT NULL,
+  `last_name` varchar(15) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `social` int(9) DEFAULT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `people`
+--
+
+INSERT INTO `people` (`id`, `first_name`, `last_name`, `address`, `email`, `phone`, `social`, `username`, `password`) VALUES
+(0000000003, 'john', 'smith', '1234 happy way', 'john@smith.com', '(619) 983-1883', 123121234, 'johnnyboi', '*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE IF NOT EXISTS `questions` (
+  `quiz_id` int(10) unsigned zerofill NOT NULL,
+  `quest_num` int(11) NOT NULL,
+  `quest_txt` varchar(200) NOT NULL,
+  `ansA` varchar(30) DEFAULT NULL,
+  `ansB` varchar(30) DEFAULT NULL,
+  `ansC` varchar(30) DEFAULT NULL,
+  `ansD` varchar(30) DEFAULT NULL,
+  `ansE` varchar(30) DEFAULT NULL,
+  `correctAnswer` char(1) DEFAULT NULL,
+  PRIMARY KEY (`quiz_id`,`quest_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `questions`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quizzes`
+--
+
+CREATE TABLE IF NOT EXISTS `quizzes` (
+  `id` int(10) unsigned zerofill NOT NULL,
+  `course_id` varchar(6) NOT NULL,
+  `isOpen` int(1) NOT NULL,
+  PRIMARY KEY (`id`,`course_id`),
+  KEY `course_id` (`course_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `quizzes`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sections`
+--
+
+CREATE TABLE IF NOT EXISTS `sections` (
+  `id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `course_id` varchar(6) NOT NULL,
+  `sec` varchar(6) NOT NULL,
+  PRIMARY KEY (`course_id`,`sec`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `sections`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE IF NOT EXISTS `students` (
+  `student_id` int(10) unsigned zerofill NOT NULL,
+  `sec_id` int(10) unsigned zerofill NOT NULL,
+  PRIMARY KEY (`student_id`,`sec_id`),
+  KEY `sec_id` (`sec_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `students`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `submissions`
+--
+
+CREATE TABLE IF NOT EXISTS `submissions` (
+  `sec_id` int(10) unsigned zerofill NOT NULL,
+  `ass_id` varchar(6) NOT NULL,
+  `student_id` int(10) unsigned zerofill NOT NULL,
+  `points_received` int(10) NOT NULL,
+  PRIMARY KEY (`sec_id`,`ass_id`,`student_id`),
+  KEY `ass_id` (`ass_id`),
+  KEY `student_id` (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `submissions`
+--
+
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `absences`
+--
+ALTER TABLE `absences`
+  ADD CONSTRAINT `absences_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `people` (`id`),
+  ADD CONSTRAINT `absences_ibfk_1` FOREIGN KEY (`sec_id`) REFERENCES `sections` (`id`);
+
+--
+-- Constraints for table `assignments`
+--
+ALTER TABLE `assignments`
+  ADD CONSTRAINT `assignments_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`);
+
+--
+-- Constraints for table `emails`
+--
+ALTER TABLE `emails`
+  ADD CONSTRAINT `emails_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `people` (`id`),
+  ADD CONSTRAINT `emails_ibfk_2` FOREIGN KEY (`parent`) REFERENCES `emails` (`id`);
+
+--
+-- Constraints for table `grades`
+--
+ALTER TABLE `grades`
+  ADD CONSTRAINT `grades_ibfk_2` FOREIGN KEY (`sec_id`) REFERENCES `sections` (`id`),
+  ADD CONSTRAINT `grades_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `people` (`id`);
+
+--
+-- Constraints for table `instructors`
+--
+ALTER TABLE `instructors`
+  ADD CONSTRAINT `instructors_ibfk_2` FOREIGN KEY (`sec_id`) REFERENCES `sections` (`id`),
+  ADD CONSTRAINT `instructors_ibfk_1` FOREIGN KEY (`instructor_id`) REFERENCES `people` (`id`);
+
+--
+-- Constraints for table `questions`
+--
+ALTER TABLE `questions`
+  ADD CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`quiz_id`) REFERENCES `quizzes` (`id`);
+
+--
+-- Constraints for table `quizzes`
+--
+ALTER TABLE `quizzes`
+  ADD CONSTRAINT `quizzes_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`);
+
+--
+-- Constraints for table `sections`
+--
+ALTER TABLE `sections`
+  ADD CONSTRAINT `sections_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`);
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`sec_id`) REFERENCES `sections` (`id`),
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `people` (`id`);
+
+--
+-- Constraints for table `submissions`
+--
+ALTER TABLE `submissions`
+  ADD CONSTRAINT `submissions_ibfk_3` FOREIGN KEY (`student_id`) REFERENCES `people` (`id`),
+  ADD CONSTRAINT `submissions_ibfk_1` FOREIGN KEY (`sec_id`) REFERENCES `sections` (`id`),
+  ADD CONSTRAINT `submissions_ibfk_2` FOREIGN KEY (`ass_id`) REFERENCES `assignments` (`id`);
