32fd97fd44df42f094ef0d67f98796453eaa88ad
hp-laptop <sorabji52@gmail.com>
2011-11-14 06:47:05 -0800

    getting there

--- capstone/views.py
+++ capstone/views.py
@@ -17,7 +17,7 @@ def get_hashed(val):
     return hasher.hexdigest()
 
 def db_connect():
-    g.db = MySQLdb.connect(host='localhost', user't3st3r', passwd='123qwe', db='junk')
+    g.db = MySQLdb.connect(host='localhost', user='t3st3r', passwd='123qwe', db='junk')
     g.cur = g.db.cursor()
 
 def user_auth(user,pw):
@@ -45,13 +45,12 @@ def login():
 @app.route('/register', methods=['POST','GET'])
 def register():
     error = None
+    db_connect()
     if 'POST' == request.method:
-        db_connect()
         pw = get_hashed(request.form['password'])
-        
-        
-    
-=======
+    else:
+        tabs = g.cur.execute('select * from people')
+        return render_template('register.html', tabs=tabs)
 
 # app.config.from_object(__name__)
 
@@ -94,33 +93,33 @@ def register():
 #             db.cursor().executescript(f.read())
 #         db.commit()
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if request.form['username'] != app.config['USERNAME']:
+#             error = 'Invalid username'
+#         elif request.form['password'] != app.config['PASSWORD']:
+#             error = 'Invalid password'
+#         else:
+#             session['logged_in'] = True
+#             flash('You were logged in')
+#             return redirect(url_for('show_entries'))
+#     return render_template('login.html', error=error)
 
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
+# @app.route('/logout')
+# def logout():
+#     session.pop('logged_in', None)
+#     flash('You were logged out')
+#     return redirect(url_for('show_entries'))
 
-@app.route('/')
-def show_entries():
-    # cur = g.db.execute('select title, text from entries order by id desc')
-    # entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    # return render_template('show_entries.html', entries=entries)
-    #return render_template('show_entries.html', {'this':'that'})
-    return "hi"
+# @app.route('/')
+# def show_entries():
+#     # cur = g.db.execute('select title, text from entries order by id desc')
+#     # entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+#     # return render_template('show_entries.html', entries=entries)
+#     #return render_template('show_entries.html', {'this':'that'})
+#     return "hi"
 
 # @app.route('/add/', methods=['POST'])
 # def add_entry():
