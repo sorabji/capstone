d77df3fb80cf982ba06673a7ae05d7e32f8dfdc9
hp-laptop <sorabji52@gmail.com>
2011-11-14 06:24:41 -0800

    sdkfj

--- capstone/__init__.py
+++ capstone/__init__.py
@@ -1,8 +1,5 @@
 from __future__ import with_statement
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
-from contextlib import closing
+from flask import Flask
 
 app = Flask(__name__)
 
--- capstone/views.py
+++ capstone/views.py
@@ -1,82 +1,53 @@
+import hashlib
+import MySQLdb
 from capstone import app
+from flask import request, session, g, redirect, url_for, \
+     abort, render_template, flash
 
-app.config.from_object(__name__)
+def get_hashed(val):
+    """returns an md5 hashed version of val.
 
-DATABASE = '/tmp/flaskr.db'
-DEBUG = True
-SECRET_KEY = 'l33t'
-USERNAME = 'admin'
-PASSWORD = '123qwe'
+    :param val: value to be hashed
+    :type val: string
+    :rtype: string
+    :returns: the hashed string
+    """
+    hasher = hashlib.md5()
+    hasher.update(val)
+    return hasher.hexdigest()
 
-@app.before_request 
-def before_request():
-    '''
-    called before request, passed no args
-    '''
-    #g.db = connect_db()
-    pass
+def db_connect():
+    g.db = MySQLdb.connect(host='localhost', user't3st3r', passwd='123qwe', db='junk')
+    g.cur = g.db.cursor()
 
-# @app.after_request
-# def hrm(resp):
-#     '''
-#     called after request, passed the response object...
-#     must return a response object
-#     '''
-#     pass
+def user_auth(user,pw):
+    """checks whether a user/pw combo exists in the users collection.
 
-@app.teardown_request
-def teardown_request(exception):
-    '''
-    called after response object has been constructed, even on error
-    '''
-    #g.db.close()
-    pass
+    :param user: user name
+    :type user: string
+    :param pw: user password
+    :type pw: string
+    :rtype: bool
+    :returns: bool indicating if auth was successful.
+    """
+    # pw    = get_hashed(pw)
+    # valid = db.users.find_one({'name':user,'pw':pw})
+    # return True if valid else False
+    pass 
 
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST','GET'])
 def login():
+    # error = None
+    # if request.method == 'POST':
+    # return render_template('login.html')
+    pass
+
+@app.route('/register', methods=['POST','GET'])
+def register():
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
-
-@app.route('/')
-def show_entries():
-    # cur = g.db.execute('select title, text from entries order by id desc')
-    # entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    # return render_template('show_entries.html', entries=entries)
-    return render(template('show_entries.html'), {'this':'that'})
-
-@app.route('/add/', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    g.db.execute('insert into entries (title,text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
-    g.db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
-
-if __name__ == '__main__':
-    app.run()
+    if 'POST' == request.method:
+        db_connect()
+        pw = get_hashed(request.form['password'])
+        
+        
+    
