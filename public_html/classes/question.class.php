9384b5003d6b43edea5b2bb92e32c12082a62107
hp-laptop <sorabji52@gmail.com>
2011-11-24 18:29:25 -0800

    gentle work on the question class

---
+++ public_html/classes/question.class.php
@@ -0,0 +1,133 @@
+<?php
+class Question extends Table{
+
+  private $list_headers = array(
+    "Quiz",
+    "Question #",
+    "Question",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "Answer");
+
+  private $list_table_cols = array(
+    "quiz_id",
+    "quest_num",
+    "quest_txt",
+    "ansA",
+    "ansB",
+    "ansC",
+    "ansD",
+    "ansE",
+    "correctAnswer" );
+
+  private $new_labels = array(
+    "Quiz",
+    "Question #",
+    "Question",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "Answer");
+
+  private $new_post_vars = array(
+    "quiz_id",
+    "quest_num",
+    "quest_txt",
+    "ansA",
+    "ansB",
+    "ansC",
+    "ansD",
+    "ansE",
+    "correctAnswer" );
+
+  private $new_help_txt = array(
+    "quiz_id",
+    "quest_num",
+    "quest_txt",
+    "ansA",
+    "ansB",
+    "ansC",
+    "ansD",
+    "ansE",
+    "correctAnswer" );
+
+  const ID = array("quiz_id", "quest_num"); // what's the id field of this table?
+
+  public function __construct($ed_flag){
+    $this->ed_flag = $ed_flag; // want to update/delete?
+  }
+
+  public function list_display($resource){
+    echo "<ul>\n";
+    while($row = mysql_fetch_array($resource)){
+      echo "<li>\n<table border='1'>\n";
+      foreach($new_post_vars as $key => $val){
+	$row[$key] = prep_sql($row[$key]);
+	echo "<tr><td>$this->$val</td>\n";
+	echo "<td>$row[$key]</td></tr>\n";
+      }
+      echo "</table></li>\n";
+    }
+    echo "</ul>\n";
+  }
+
+  public function new_display(){
+    echo "<form id='form' name='form' action='' method='POST'>\n";
+    echo "<p>All fields are required</p>\n";
+    echo "<fieldset><legend>Person Info</legend>\n";
+    echo "<div class='notes'>\n";
+    echo "<h4>notes</h4>\n";
+    echo "<p class='last'>fill it out properly...all of it damnit!</p>\n</div>\n";
+
+    foreach($this->new_labels as $key => $val){
+      echo "<div class='required'>\n";
+      echo "<label for='{$this->new_post_vars[$key]}'>";
+      echo $this->new_labels[$key];
+      echo "</label>\n";
+
+      if ( (strcmp($this->new_post_vars[$key], "password") == 0) |
+	strcmp($this->new_post_vars[$key], "password_2") == 0 ){
+
+      	echo "<input class='inputPassword' type='password' name='{$this->new_post_vars[$key]}' id='{$this->new_post_vars[$key]}' />\n";
+      } else {
+	echo "<input class='inputText' type='text' name='{$this->new_post_vars[$key]}' id='{$this->new_post_vars[$key]}' />\n";
+      }
+    }
+    echo "<div class='submit'>\n";
+    echo "<div><input type='submit' id='submit' name='submit' class='inputSubmit' value='Add Person' />\n";
+    echo "</div>\n</div>\n</fieldset>\n</form>";
+    echo($ret);
+  }
+
+  public function edit_display($id){
+    return 0;
+  }
+
+  public function get_update_qry($vals){
+
+    // don't forget to hash the damn passwords
+    foreach($_POST AS $key => $value) { $_POST[$key] = $this->prep_sql($value); }
+
+    $base = "INSERT INTO `people` ( `first_name` ,  `last_name` ,  `address` ";
+    $base .= ",  `email` ,  `phone` ,  `social` ,  `username` , `password` ) ";
+    $fmt_str = "VALUES( '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s' );";
+    $res = sprintf($fmt_str, // can't just unpack?...hrm
+	   $vals[$this->new_post_vars[0]],
+    	   $vals[$this->new_post_vars[1]],
+    	   $vals[$this->new_post_vars[2]],
+    	   $vals[$this->new_post_vars[3]],
+    	   $vals[$this->new_post_vars[4]],
+    	   $vals[$this->new_post_vars[5]],
+    	   $vals[$this->new_post_vars[6]],
+    	   $vals[$this->new_post_vars[7]]);
+    $fin = $base . $res;
+    return $fin;
+  }
+
+}
+?>
