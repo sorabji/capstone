01bfb7849a921ab23e04e66f796317f71296c5f0
ratio (devvz) <sorabji52@gmail.com>
2011-12-06 22:45:34 -0800

    working on log in system...check your sql

--- public_html/classes/access.class.php
+++ public_html/classes/access.class.php
@@ -24,8 +24,8 @@ class Access{
 
     $sql = "select `username`, `password` from people where username='".$user."' and password='".$pass."';";
     $res = mysql_query($sql);
+    echo mysql_error();
     if(mysql_num_rows($res) != 0){
-      $_SESSION['isAuthorized'] = true;
       $_SESSION['user'] = $user;
       header("Location: index.php");
       exit();
@@ -37,19 +37,59 @@ class Access{
     /*
       check if they logged in
      */
+    return (isset($_SESSION['user'])) ? true : false;
   }
 
   public function logout(){
     /*
       log those bitchez out
     */
+    unset($_SESSION['user']);
   }
 
   public function get_user_a_level(){
     /*
       query tables, return 0-3 indicating user's a_level
     */
+    if(!($this->is_logged_in())) return 0;
+    $user = $_SESSION['user'];
+
+    $lim = "( select `id` from people where `username` = $user );";
+    $hrm = "select `id` from people where `username` = $user;";
+    $three = "select `admin_id` from admins where `admin_id` = ";
+    $two = "select `instructor_id` from instructors where `instructor_id` = ";
+    $one = "select `student_id` from students where `student_id` = ";
+
+    $b = mysql_query($hrm);
+    echo($b);
+    
+    $res = mysql_query($three . $lim);
+    if($res){
+      return 3;
+    } else {
+      $res = mysql_query($two . $lim);
+      if($res){
+	return 2;
+      } else {
+	$res = mysql_query($one . $lim);
+	if($res){
+	  return 1;
+	} else {
+	  return 0;
+	}
+      }
+    }
   }
 
+  public function do_eet($root){
+    $user_a_level = $this->get_user_a_level();
+    echo $user_a_level;
+    echo $this->get_user_a_level();
+    echo $_SESSION['user'];
+    if($this->a_level > $user_a_level){
+      //header("Location: ".$root."login.php");
+      //exit();
+    }
+  }
 }
 ?>
