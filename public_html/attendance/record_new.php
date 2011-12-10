38487693ba886ac7d07b151cada36bf4208a41aa
unknown <Tay@Tay-HP.(none)>
2011-12-10 00:47:52 -0800

    more changes to absent class and php files etc. Tried to not upload .util file, did it work?

--- public_html/attendance/record_new.php
+++ public_html/attendance/record_new.php
@@ -1,31 +1,44 @@
-<link rel='stylesheet' type='text/css' href="../static/style.css" />
-<!--<link rel='stylesheet' type='text/css' href="../form_testing.css" />-->
-
 <?php
-include('../header.php');
-include('../util.php');
 
-echo "<p>Beginning of Table</p>";
+	include('../util.php');
+	include('../header.php');
 
-$att = new Daily_Record(true);
+	
+	
 
-if (isset($_POST['submit'])) { 
-//if (false){
+	echo "<p>Beginning of Table</p>";
+	//List Class
+	$link = connect();
 
-  $fin = $att->get_update_qry($_POST);
+	//dropdown menu to select course
+	echo "<select name='course'><option value=''></option>  ";
+	get_courses();
+	
+	$resource = mysql_query("SELECT * FROM absences, students, people WHERE students.id = absences.fk_absent_student AND students.student_id = people.id AND absences.the_date = '2011-12-06';", $link);
 
-  mysql_query($fin) or die(mysql_error()); 
-  //echo($fin);
-  if($fin){
-    echo "<p>Records Updated.</p><br />";
-    echo "<a href='record_list.php'>View Attendance Records</a>";
-  } else {
-    echo("nothing added");
-  }
-} else {
-  $att->new_display();
+		$absences = new Absent(true);
+		$absences->new_list_display($resource);
 
-}
-echo "<p>End of Table</p>";
-//include('../footer.php');
+	//
+	$att = new Absent(true);
+
+	if (isset($_POST['submit'])) { 
+	//if (false){
+
+	  $fin = $att->get_update_qry($_POST);
+
+	  mysql_query($fin) or die(mysql_error()); 
+	  //echo($fin);
+	  if($fin){
+		echo "<p>Records Updated.</p><br />";
+		echo "<a href='record_list.php'>View Attendance Records</a>";
+	  } else {
+		echo("nothing added");
+	  }
+	} else {
+	  $att->new_record();
+
+	}
+	echo "<p>End of Table</p>";
+	//include('../footer.php');
 ?>
