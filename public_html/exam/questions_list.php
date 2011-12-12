9055835ab632767612d9d5c7262f86332183e699
hp-laptop <sorabji52@gmail.com>
2011-12-11 21:38:15 -0800

    working on some more navigation testing functionalities

--- public_html/exam/questions_list.php
+++ public_html/exam/questions_list.php
@@ -17,11 +17,22 @@ $(function() {
 });
 </script>
 <?php
-$resource = mysql_query("select * from questions");
-	   
-$quest = new Question(true);
-$quest->list_display($resource);
 
+if(isset($_GET['q_id'])){
+  $q_id = $_GET['q_id'];
+} else {
+  $q_id = false;
+}
+
+// pass quiz id w/ get_var
+if($q_id){
+  $resource = mysql_query("select * from questions where quiz_id=$q_id");
+  $quest = new Question(true, $root);
+  $quest->list_display($resource);
+} else {
+  echo "<p class='error'>You must select a quiz</p>";
+  echo "<p>Go to the quiz selection <a href='".$root."exam/questions_search.php'>page</a> and try again</p>";
+}
 
 include_once('../footer.php');
 ?>
